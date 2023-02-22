0179a0f015a0ff3d9903958117dca03db16f8f12
CassieDoo <kse8743@yandex.ru>
2023-02-22 18:15:48 +0500

    SQL-Task_5

--- SQL-Task_5.py
+++ SQL-Task_5.py
@@ -7,15 +7,15 @@ def create_db(conn):
         client_id SERIAL PRIMARY KEY,
         first_name VARCHAR(40) NOT NULL,
         last_name VARCHAR(40) NOT NULL,
-        email VARCHAR(40) NOT NULL
+        email VARCHAR(40) NOT NULL UNIQUE
     );
     """)
 
     cur.execute("""
     CREATE TABLE IF NOT EXISTS phone(
         phone_id SERIAL PRIMARY KEY,
-        phone_number VARCHAR(40),
-        client_id INTEGER NOT NULL REFERENCES client(client_id)
+        phone_number VARCHAR(40) UNIQUE,
+        client_id INTEGER NOT NULL REFERENCES client(client_id) ON DELETE CASCADE
     );
     """)
     conn.commit()
@@ -30,47 +30,75 @@ def inuput_data():
 
 
 def add_client(conn, first_name, last_name, email, phone_number=None):
-    cur.execute("""
-            INSERT INTO client (first_name, last_name, email) VALUES (%s, %s, %s) RETURNING client_id;
-            """, (first_name, last_name, email))
-    client_id = cur.fetchone()[0]
+    if find_client(conn, email=email):
+        return "Клиент с такой почтой уже существует"
 
-    while True:
-        if phone_number:
-            add_phone(conn, client_id, phone_number)
-            phone_number = input("Введите добавляемый номер телефона: ")
-        else:
-            break
-        conn.commit()
+    cur.execute(
+        """
+        INSERT INTO client (first_name, last_name, email)
+        VALUES (%s, %s, %s) RETURNING client_id;
+        """, (first_name, last_name, email))
+
+    if phone_number:
+        client_id = cur.fetchone()[0]
+
+        x = add_phone(conn, client_id, phone_number)
+        if x == 'Номер уже существует':
+            conn.rollback()
+            return "Номер уже существует, добавление невозможно"
+
+    conn.commit()
+    return "Клиент добавлен"
 
 
 def add_phone(conn, client_id, phone_number):
     cur.execute("""
+        SELECT phone_number FROM phone
+        WHERE phone_number = %s;
+        """, (phone_number,))
+
+    if cur.fetchone():
+        return "Номер уже существует"
+
+    cur.execute("""
         INSERT INTO phone (client_id, phone_number) VALUES (%s, %s);
         """, (client_id, phone_number))
+
     conn.commit()
+    return "Телефон добавлен"
 
 
 def change_client(conn, client_id, first_name=None, last_name=None, email=None, phone_number=None):
     if first_name:
         cur.execute("""
-            UPDATE client SET first_name=%s WHERE client_id=%s;
+            UPDATE client SET first_name=%s
+            WHERE client_id=%s;
             """, (first_name, client_id))
-        return conn.commit()
+        conn.commit()
 
     if last_name:
         cur.execute("""
-            UPDATE client SET last_name=%s WHERE client_id=%s;
+            UPDATE client SET last_name=%s
+            WHERE client_id=%s;
             """, (last_name, client_id))
-        return conn.commit()
+        conn.commit()
 
     if email:
         cur.execute("""
-            UPDATE client SET email=%s WHERE client_id=%s;
+            UPDATE client SET email=%s
+            WHERE client_id=%s;
             """, (email, client_id))
-        return conn.commit()
+        conn.commit()
 
     if phone_number:
+        cur.execute("""
+                SELECT phone_number FROM phone
+                WHERE phone_number = %s;
+                """, (phone_number,))
+
+        if cur.fetchone():
+            return "Номер уже существует"
+
         old_phone_number = input("Выберете номер телефона для изменения: ")
         cur.execute("""
                 SELECT phone_id, client_id FROM phone
@@ -79,51 +107,65 @@ def change_client(conn, client_id, first_name=None, last_name=None, email=None, 
         phone_id = cur.fetchone()[0]
 
         cur.execute("""
-            UPDATE phone SET phone_number=%s WHERE phone_id=%s;
+            UPDATE phone SET phone_number=%s
+            WHERE phone_id=%s;
             """, (phone_number, phone_id))
-        return conn.commit()
+        conn.commit()
+
+    return "Данные клиента изменены"
 
 
 def delete_phone(conn, client_id, phone_number):
     cur.execute("""
-        SELECT phone_id, client_id FROM phone
-        WHERE client_id=%s AND phone_number=%s;
+        DELETE FROM phone
+        WHERE client_id=%s and phone_number=%s
+        RETURNING *;
         """, (client_id, phone_number))
-    phone_id = cur.fetchone()[0]
+    if not cur.fetchone():
+        return "Такого номера нет"
 
-    cur.execute("""
-        DELETE FROM phone WHERE phone_id=%s;
-        """, (phone_id,))
     conn.commit()
-
+    return "Телефон удален"
 
 def delete_client(conn, client_id):
     cur.execute("""
-        DELETE FROM phone WHERE client_id=%s;
+        DELETE FROM client
+        WHERE client_id=%s;
         """, (client_id,))
-    conn.commit()
 
-    cur.execute("""
-        DELETE FROM client WHERE client_id=%s;
-        """, (client_id,))
     conn.commit()
+    return "Клиент удален"
 
 
 def find_client(conn, first_name=None, last_name=None, email=None, phone_number=None):
-    cur.execute("""
-        SELECT client.client_id, client.first_name, client.last_name, client.email, phone.phone_number FROM client
-        LEFT JOIN phone ON client.client_id = phone.client_id
-        WHERE first_name=%s OR last_name=%s OR email=%s OR phone_number=%s;
-        """, (first_name, last_name, email, phone_number))
-    client = cur.fetchall()
-    if not client:
-        print("Клиент не найден")
-    else:
-        return client
+    client_list = [first_name, last_name, email]
+    for index, value in enumerate(client_list):
+        if not value:
+            client_list[index] = '%'
+    phone_search = ""
+    if phone_number:
+        phone_search = "AND ARRAY_AGG(phone.phone_number)::TEXT[] && ARRAY[%s]"
+        client_list.append(phone_number)
+    query = f"""
+                SELECT client.client_id, client.first_name, client.last_name, client.email,
+                    CASE
+                        WHEN ARRAY_AGG(phone.phone_number) = '{{Null}}' THEN ARRAY[]::TEXT[]
+                        ELSE ARRAY_AGG(phone.phone_number)
+                    END phone
+                FROM client
+                LEFT JOIN phone ON client.client_id = phone.client_id
+                GROUP BY client.client_id, client.first_name, client.last_name, client.email
+                HAVING client.first_name LIKE %s AND client.last_name LIKE %s AND client.email LIKE %s {phone_search};
+                """
+    cur.execute(
+        query,
+        client_list)
+    return cur.fetchall()
 
 
-with psycopg2.connect(database="clients_db", user="postgres", password="postgres") as conn:
+with psycopg2.connect(database="clients_db", user="postgres", password="050700793") as conn:
     with conn.cursor() as cur:
+
         create_db(conn)
 
         command = input("Введите команду: ").lower()
@@ -131,7 +173,7 @@ with psycopg2.connect(database="clients_db", user="postgres", password="postgres
         if command == "ac":
             print("Введите данные для добавления клиента: ")
             client_details = inuput_data()
-            add_client(conn, client_details[0], client_details[1], client_details[2], client_details[3])
+            print(add_client(conn, client_details[0], client_details[1], client_details[2], client_details[3]))
 
         else:
             print("Введите данные для поиска клиента: ")
@@ -140,26 +182,26 @@ with psycopg2.connect(database="clients_db", user="postgres", password="postgres
 
             if command == "ap":
                 phone_number = input("Введите добавляемый номер телефона: ")
-                add_phone(conn, client[0][0], phone_number)
+                print(add_phone(conn, client[0][0], phone_number))
 
             elif command == "cc":
                 if client:
                     print(client)
                     print("Введите новые данные клиента:")
                     new_client_details = inuput_data()
-                    change_client(conn, client[0][0], new_client_details[0], new_client_details[1],
-                                  new_client_details[2], new_client_details[3])
+                    print(change_client(conn, client[0][0], new_client_details[0], new_client_details[1],
+                                        new_client_details[2], new_client_details[3]))
 
             elif command == "dp":
                 if client:
                     print(client)
                     phone_number = input("Выберете номер телефона для удаления: ")
-                    delete_phone(conn, client[0][0], phone_number)
+                    print(delete_phone(conn, client[0][0], phone_number))
 
             elif command == "dc":
                 if client:
                     print(client)
-                    delete_client(conn, client[0][0])
+                    print(delete_client(conn, client[0][0]))
 
             elif command == "fc":
                 print(client)
